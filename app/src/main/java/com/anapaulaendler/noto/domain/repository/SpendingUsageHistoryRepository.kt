a7153e0c3aa33fb49854015f9dd8f07601ed48a1
Ana Paula Endler <anapaulaendler@gmail.com>
2025-08-20 10:59:10 -0300

    feat: domain/repository SpendingUsageHistoryRepository

---
+++ app/src/main/java/com/anapaulaendler/noto/domain/repository/SpendingUsageHistoryRepository.kt
@@ -0,0 +1,5 @@
+package com.anapaulaendler.noto.domain.repository
+
+import com.anapaulaendler.noto.domain.model.SpendingUsageHistory
+
+interface SpendingUsageHistoryRepository: BaseRepository<SpendingUsageHistory>
