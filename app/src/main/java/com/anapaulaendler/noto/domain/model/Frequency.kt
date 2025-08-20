cb5ddbcf6555d5b76d6efb953ce8b49a684bbe97
Ana Paula Endler <anapaulaendler@gmail.com>
2025-08-19 21:49:20 -0300

    fix: frequency weekdaymask Int -> Int?

--- app/src/main/java/com/anapaulaendler/noto/domain/model/Frequency.kt
+++ app/src/main/java/com/anapaulaendler/noto/domain/model/Frequency.kt
@@ -7,7 +7,7 @@ data class Frequency(
     val id: Long,
     val type: RecurrenceType,
     val interval: Int,
-    val weekdayMask: Int,
+    val weekdayMask: Int?,
     val monthDay: Int?,
     val nthWeek: Int?,
     val weekDay: Weekday?,
